2a6bcb735d789e69ba8cfd8a03f2aab900e03262
jayfunfashion <168953966+jayfunfashion@users.noreply.github.com>
2026-01-05 15:06:37 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -13,6 +13,7 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  <script type="module" src="/index.tsx">
 );
-
+<body>
+<script type="module" src="/index.tsx"></script>
+</body>
